3dc858279f63f0a3a3a26ca6173766ca5cbeda39
Vancios86 <albuion86@gmail.com>
2022-01-06 19:18:59 +0000

    animate the title, stylistic improvements

--- src/index.js
+++ src/index.js
@@ -3,18 +3,16 @@ import initSr from './js/sr';
 
 import './style/main.scss';
 
-$('a[href^="#"]').on('click', function(event) {
+$('a[href^="#"]').on('click', function (event) {
   var target = $(this.getAttribute('href'));
   if (target.length) {
     event.preventDefault();
-    $('html, body')
-      .stop()
-      .animate(
-        {
-          scrollTop: target.offset().top
-        },
-        1000
-      );
+    $('html, body').stop().animate(
+      {
+        scrollTop: target.offset().top,
+      },
+      1000
+    );
   }
 });
 
